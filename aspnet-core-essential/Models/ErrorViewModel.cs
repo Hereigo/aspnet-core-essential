86a324606f79092a2da3f8dfff811a14bca9a722
Hereigo <Hereigo@online.ua>
2018-09-10 14:55:33 +0300

    init project with middleware

---
+++ aspnet-core-essential/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aspnet_Core_Essential.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ aspnet-core-essential/MyCustomMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Aspnet_Core_Essential
+{
+	public class MyCustomMiddleware
+	{
+		// 1. Middleware must have RequestDelegate _link2next:
+		private readonly RequestDelegate _next;
+
+		// 2. public Ctor :
+		public MyCustomMiddleware(RequestDelegate nextMiddleWare)
+		{
+			_next = nextMiddleWare;
+		}
+
+		// 3. Method named as Invoke/InvokeAsync :
+		public async Task InvokeAsync(HttpContext httpContext)
+		{
+			if (httpContext.Request.Method == "POST")
+			{
+				await httpContext.Response.WriteAsync("it was POST request");
+			}
+			else
+			{
+				await _next.Invoke(httpContext);
+			}
+		}
+
+		// 4. Using - app.UseMiddleware < MyCustomMiddleware > ();
+	}
+}
